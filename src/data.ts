7b10d1261d621211e9f010197e2ed0e69759929c
Akshaya Kumar Puvvada <akk.puvvada@gmail.com>
2025-03-16 21:14:51 -0500

    Adding data and defining the interface

---
+++ src/data.ts
@@ -0,0 +1,81 @@
+import type { TableRow } from "../types";
+
+// Sample data for "Corporate information"
+export const corporateData: TableRow[] = [
+    {
+        no: 1,
+        item: "The company's background, organization charts, and description of major function(s)/ responsibility of key officers.",
+        dateObtained: "3/14/2025",
+        tag: "Lorem-1p2",
+        index: "1.0",
+        notes: "Please provide in excel format",
+        hyperlink: "Select file",
+    },
+    {
+        no: 2,
+        item: "Business license(s)/ registration",
+        dateObtained: "3/14/2025",
+        tag: "Lorem-1p2",
+        index: "2.0",
+        notes: "Please provide in excel format",
+        hyperlink: "Select file",
+    },
+    {
+        no: 3,
+        item: "Articles of incorporation, bylaws, and any amendments",
+        dateObtained: "3/14/2025",
+        tag: "Lorem-1p2",
+        index: "3.0",
+        notes: "Please provide in excel format",
+        hyperlink: "Select file",
+    },
+    {
+        no: 4,
+        item: "Minutes/ resolutions of the board of directors",
+        dateObtained: "3/14/2025",
+        tag: "Lorem-1p2",
+        index: "4.0",
+        notes: "Please provide in excel format",
+        hyperlink: "Select file",
+    },
+];
+
+// Sample data for "Financial information"
+export const financialData: TableRow[] = [
+    {
+        no: 1,
+        item: "The company's background, organization charts, and description of major function(s)/ responsibility of key officers.",
+        dateObtained: "3/14/2025",
+        tag: "Lorem-1p2",
+        index: "1.0",
+        notes: "Please provide in excel format",
+        hyperlink: "Select file",
+    },
+    {
+        no: 2,
+        item: "Business license(s)/ registration",
+        dateObtained: "3/14/2025",
+        tag: "Lorem-1p2",
+        index: "2.0",
+        notes: "Please provide in excel format",
+        hyperlink: "Select file",
+    },
+    {
+        no: 3,
+        item: "Articles of incorporation, bylaws, and any amendments",
+        dateObtained: "3/14/2025",
+        tag: "Lorem-1p2",
+        index: "3.0",
+        notes: "Please provide in excel format",
+        hyperlink: "Select file",
+    },
+    {
+        no: 4,
+        item: "Minutes/ resolutions of the board of directors",
+        dateObtained: "3/14/2025",
+        tag: "Lorem-1p2",
+        index: "4.0",
+        notes: "Please provide in excel format",
+        hyperlink: "Select file",
+    },
+];
---
+++ types.ts
@@ -0,0 +1,9 @@
+export interface TableRow {
+    no: number;
+    item: string;
+    dateObtained: string;
+    tag: string;
+    index: string;
+    notes: string;
+    hyperlink: string;
+}
